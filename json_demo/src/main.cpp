3abaa8d479cceb33ba4cfae02b62b48e3c73f74a
lehaifeng <lehaifeng000@163.com>
2023-09-27 10:00:58 +0800

    配置cmake，优化json示例

--- json_demo/src/main.cpp
+++ json_demo/src/main.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
 #include "json.hpp"
+#include <fstream>
 
 using json = nlohmann::json;
 using namespace std;
 int main()
 {
+    //json -> file
     cout<<"hello"<<endl;
     json data;
     data["name"]="lhf";
     data["age"]=27;
     data["city"]="beijing";
     std::ofstream file("data.json");
-    file << data
+    //直接写入，无缩进
+//    file << data;
+    //格式化写入
+    file << data.dump(4);
+    file.flush();
+    file.close();
+
+    //file-> json
+    ifstream inFile("data.json");
+    json json2 = json::parse(inFile);
+    cout<<json2["age"];
+
     return 0;
 }
 
